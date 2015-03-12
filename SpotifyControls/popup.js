0c02fd64f94bd2ab36cc8276e5924537dcb7c5de
Sergej Popov <sergejpopov@outlook.com>
2015-03-12 22:43:08 +0000

    Initial Version 1.1.1

---
+++ SpotifyControls/popup.js
@@ -0,0 +1,60 @@
+(function($, undefined){ 
+
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var controls = document.getElementsByClassName('control');
+		
+		for(var i = 0; i < controls.length; i++){
+			controls[i].addEventListener('click', function(evt) {
+				activateControl(evt.srcElement.id)
+			});
+		}
+		
+		//setInterval(function(){
+			updateTrackInfo();
+		//}, 1000);
+		
+		
+	});
+
+	function updateTrackInfo()
+	{
+		var queryInfo = {
+			url: "https://play.spotify.com/*"
+		};
+		chrome.tabs.query(queryInfo, function(tabs) {
+			if(tabs.length < 1)
+				return;
+			
+			var tabId = tabs[0].id;
+			chrome.tabs.executeScript(tabId, {file:'spotify.getTrackInfo.js'}, function(track){
+				console.log(track)
+				$('#track-artist').html(track[0][0].artist);
+				$('#track-name').html(track[0][0].name);
+				$('#track-art').attr('src', track[0][0].art.slice(4, track[0][0].art.length-1 ));
+				$('#track-progress').width(track[0][0].progress);
+				$('#track-current').html(track[0][0].track_current);
+				$('#track-length').html(track[0][0].track_length);
+			});
+			
+		});
+	}
+	  
+	function activateControl(control)
+	{
+		var queryInfo = {
+			url: "https://play.spotify.com/*"
+		};
+
+		chrome.tabs.query(queryInfo, function(tabs) {
+			if(tabs.length < 1)
+				return;
+			
+			var tabId = tabs[0].id;
+			var script = "document.getElementById('app-player').contentWindow.document.getElementById('" + control + "').click()";
+			
+			chrome.tabs.executeScript(tabId, {code:script});
+			
+		});
+	}
+ }(jQuery));
